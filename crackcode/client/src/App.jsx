85f82416d370f6425bd8199cb70c303a8bb352d0
Ama-Dombawela <amadombawela@gmail.com>
2025-12-22 17:18:30 +0530

    setup profile styling-game/user

--- crackcode/client/src/App.jsx
+++ crackcode/client/src/App.jsx
@@ -1,11 +1,13 @@
 import {Routes,Route} from 'react-router-dom'
 import UserProfile from './pages/userprofile/userprofile'
+import GameProfile from './pages/gameprofile/gameprofile'
 import './App.css'
 
 function App() {
 
   return (
     <Routes>
+        <Route path ="/gameprofile" element={<GameProfile/>} />
         <Route path ="/userprofile" element={<UserProfile/>} />
     </Routes>
     
