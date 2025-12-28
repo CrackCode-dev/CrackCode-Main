291470a1ff409ff10ad22378a06f7cc6eb35d6ce
Ama-Dombawela <amadombawela@gmail.com>
2025-12-28 17:29:45 +0530

    Added authentication pages(5) and authentication context file

--- crackcode/client/src/App.jsx
+++ crackcode/client/src/App.jsx
@@ -3,12 +3,16 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/home/Home'
 import Landing from './pages/landing/Landing'
+import GameProfile from './pages/gameprofile/gameprofile'
+import UserProfile from './pages/userprofile/userprofile'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Landing />} />
       <Route path='/home' element={<Home />} />
+      <Route path='/gameprofile' element={<GameProfile />} />
+      <Route path='/userprofile' element={<UserProfile/>} />
     </Routes>
   )
 }
