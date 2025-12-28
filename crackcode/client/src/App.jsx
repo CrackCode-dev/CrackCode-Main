4740ce2b20a73b3ec36410211fe91e63f00f01a3
Ama-Dombawela <amadombawela@gmail.com>
2025-12-28 14:30:59 +0530

    made the errors with routing and pull changes

--- crackcode/client/src/App.jsx
+++ crackcode/client/src/App.jsx
@@ -1,20 +1,15 @@
-import {Routes,Route} from 'react-
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/home/Home'
 import Landing from './pages/landing/Landing'
-import UserProfile from './pages/userprofile/userprofile'
-import GameProfile from './pages/gameprofile/gameprofile'
 
 function App() {
-
   return (
     <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/home' element={<Home />} />
-        <Route path ="/gameprofile" element={<GameProfile/>} />
-        <Route path ="/userprofile" element={<UserProfile/>} />
+      <Route path='/' element={<Landing />} />
+      <Route path='/home' element={<Home />} />
     </Routes>
-    
   )
 }
 
