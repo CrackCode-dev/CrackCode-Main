0ed2982396537f87843ee3eb6f89301925f089cf
Ama-Dombawela <amadombawela@gmail.com>
2025-12-29 22:07:18 +0530

    Added bacground auth-bg(video) for authentication pages

--- crackcode/client/src/pages/userauth/Login.jsx
+++ crackcode/client/src/pages/userauth/Login.jsx
@@ -4,7 +4,7 @@ import { AppContent } from '../../context/userauth/authenticationContext'
 import Button from '../../components/common/Button'
 import axios from 'axios'
 import { toast } from 'react-toastify'
-import {UserRound, Mail,LockKeyhole } from 'lucide-react'
+import { UserRound, Mail, LockKeyhole } from 'lucide-react'
 
 
 function Login() {
@@ -28,24 +28,24 @@ function Login() {
                 if (data.success) {
                     setIsLoggedIn(true)
                     getUserData()
-                    
+
                     try {
                         const otpResponse = await axios.post(backendUrl + '/api/auth/send-verify-otp');
-                        
+
                         if (otpResponse.data.success) {
                             toast.success("Account created! OTP sent to email.");
-                           
+
                             navigate('/email-verify');
                         } else {
                             toast.error(otpResponse.data.message);
-                            navigate('/'); 
+                            navigate('/');
                         }
                     } catch (error) {
                         toast.error("Failed to send OTP: " + error.message);
                         navigate('/');
                     }
-                    
-                    
+
+
                 } else {
                     toast.error(data.message)
                 }
@@ -65,20 +65,30 @@ function Login() {
     }
 
     return (
-        
-        <div className='flex felx-col items-center justify-center min-h-screen bg-[#050505]'>
+
+        <div className='flex flex-col items-center justify-center min-h-screen overflow-hidden'>
+
+            {/*Background video*/}
+            <video autoPlay loop muted playsInline className='absolute inset-0 w-full h-full object-cover z-0'>
+                <source src="/auth-bg.mp4" type="video/mp4" />
+            </video>
+
+            {/* Dark overlay */}
+            <div className="absolute inset-0 bg-black/60 z-10"></div>
+
+            
             <div onClick={() => navigate('/')} className="w-full absolute top-0 cursor-pointer">
-               
+
             </div>
 
-            <div className='bg-[#121212] p-10 rounded-lg shadow-lg w-f sm:w-96 text-orange-400 text-sm'>
+            <div className='bg-[#121212] z-20 p-10 rounded-lg shadow-lg w-full sm:w-96 text-orange-400 text-sm'>
                 <h2 className='text-3xl font-semibold text-white text-center mb-3'>{state === 'Sign Up' ? 'Create Account' : 'Login'}</h2>
                 <p className='text-center text-sm mb-6'>{state === 'Sign Up' ? 'Create your account' : 'Login to your account!'}</p>
 
                 <form onSubmit={onSubmitHandler}>
                     {state === 'Sign Up' && (
                         <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-gray-500'>
-                            <UserRound className='w-5 h-5 text-gray-400'/>
+                            <UserRound className='w-5 h-5 text-gray-400' />
                             <input onChange={e => setName(e.target.value)}
                                 value={name}
                                 className='bg-transparent outline-none text-white' type="text" placeholder='Full Name' required />
@@ -86,14 +96,14 @@ function Login() {
                     )}
 
                     <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-gray-500'>
-                        <Mail className='w-5 h-5 text-gray-400'/>
+                        <Mail className='w-5 h-5 text-gray-400' />
                         <input
                             onChange={e => setEmail(e.target.value)}
                             value={email} className='bg-transparent outline-none text-white' type="email" placeholder='Email id' required />
                     </div>
 
                     <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-gray-500'>
-                       <LockKeyhole className='w-5 h-5 text-gray-400'/>
+                        <LockKeyhole className='w-5 h-5 text-gray-400' />
                         <input
                             onChange={e => setPassword(e.target.value)}
                             value={password}
