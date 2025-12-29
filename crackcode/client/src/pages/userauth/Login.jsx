35208352d54eb422582ee21e03fc1979e12cf2fa
Ama-Dombawela <amadombawela@gmail.com>
2025-12-29 20:16:19 +0530

    added button styles and icons for login/email verify pages

--- crackcode/client/src/pages/userauth/Login.jsx
+++ crackcode/client/src/pages/userauth/Login.jsx
@@ -1,8 +1,11 @@
 import React, { useContext, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { AppContent } from '../../context/userauth/authenticationContext'
+import Button from '../../components/common/Button'
 import axios from 'axios'
 import { toast } from 'react-toastify'
+import {UserRound, Mail,LockKeyhole } from 'lucide-react'
+
 
 function Login() {
 
@@ -68,14 +71,14 @@ function Login() {
                
             </div>
 
-            <div className='bg-[#121212] p-10 rounded-lg shadow-lg w-f sm:w-96 text-[#018801] text-sm'>
+            <div className='bg-[#121212] p-10 rounded-lg shadow-lg w-f sm:w-96 text-orange-400 text-sm'>
                 <h2 className='text-3xl font-semibold text-white text-center mb-3'>{state === 'Sign Up' ? 'Create Account' : 'Login'}</h2>
                 <p className='text-center text-sm mb-6'>{state === 'Sign Up' ? 'Create your account' : 'Login to your account!'}</p>
 
                 <form onSubmit={onSubmitHandler}>
                     {state === 'Sign Up' && (
                         <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-gray-500'>
-                            
+                            <UserRound className='w-5 h-5 text-gray-400'/>
                             <input onChange={e => setName(e.target.value)}
                                 value={name}
                                 className='bg-transparent outline-none text-white' type="text" placeholder='Full Name' required />
@@ -83,34 +86,33 @@ function Login() {
                     )}
 
                     <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-gray-500'>
-                      
+                        <Mail className='w-5 h-5 text-gray-400'/>
                         <input
                             onChange={e => setEmail(e.target.value)}
                             value={email} className='bg-transparent outline-none text-white' type="email" placeholder='Email id' required />
                     </div>
 
                     <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-gray-500'>
-                       
+                       <LockKeyhole className='w-5 h-5 text-gray-400'/>
                         <input
                             onChange={e => setPassword(e.target.value)}
                             value={password}
                             className='bg-transparent outline-none text-white' type="password" placeholder='Password' required />
                     </div>
 
-                    <p onClick={() => navigate('/resetpassword')} className='mb-4 text-[#018801] cursor-pointer'>Forgot Password</p>
+                    <p onClick={() => navigate('/resetpassword')} className='mb-4 text-orange-400 cursor-pointer'>Forgot Password</p>
 
-                    <button className='w-full py-2.5 rounded-full bg-[#018801] hover:bg-[#018801a3]
-            text-white font-medium'>{state}</button>
+                    <Button variant='primary' size='md' fullWidth type='submit' className="!rounded-full h-auto py-2"  >{state}</Button>
                 </form>
 
                 {state === 'Sign Up' ? (
                     <p className='text-gray-400 text-center text-xs mt-4'>Already have an account?{' '}
-                        <span onClick={() => setState('Login')} className='text-[#57de57] cursor-pointer underline'>Login here</span>
+                        <span onClick={() => setState('Login')} className='text-orange-400 cursor-pointer underline'>Login here</span>
                     </p>
                 )
                     : (
                         <p className='text-gray-400 text-center text-xs mt-4'>Don't have an account?{' '}
-                            <span onClick={() => setState('Sign Up')} className='text-[#57de57] cursor-pointer underline'>Sign Up</span>
+                            <span onClick={() => setState('Sign Up')} className='text-orange-400  cursor-pointer underline'>Sign Up</span>
                         </p>
                     )}
 
