ddb77394bfbb966d7ecad898157c45b48e8d7296
Ama-Dombawela <amadombawela@gmail.com>
2025-12-30 16:32:36 +0530

    Added a hover effect for the uth pages input fields

--- crackcode/client/src/pages/userauth/Login.jsx
+++ crackcode/client/src/pages/userauth/Login.jsx
@@ -89,7 +89,7 @@ function Login() {
 
                 <form onSubmit={onSubmitHandler}>
                     {state === 'Sign Up' && (
-                        <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-gray-700'>
+                        <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-gray-600 focus-within:ring-1 focus-within:ring-white transition-all duration-300 ease-in-out">
                             <UserRound className='w-5 h-5 text-gray-400' />
                             <input onChange={e => setName(e.target.value)}
                                 value={name}
@@ -97,14 +97,14 @@ function Login() {
                         </div>
                     )}
 
-                    <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-gray-700'>
+                    <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-gray-600 focus-within:ring-1 focus-within:ring-white transition-all duration-300 ease-in-out">
                         <Mail className='w-5 h-5 text-gray-400' />
                         <input
                             onChange={e => setEmail(e.target.value)}
                             value={email} className='bg-transparent outline-none text-white w-full' type="email" placeholder='Email id' required />
                     </div>
 
-                    <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-gray-700'>
+                    <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-gray-600 focus-within:ring-1 focus-within:ring-white transition-all duration-300 ease-in-out">
                         <LockKeyhole className='w-5 h-5 text-gray-400' />
                         <input
                             onChange={e => setPassword(e.target.value)}
