e8d3c3c23164dedfcb6a836f1b0dc6bd0df72e2f
Sasni Lasadi <sasnilasadi410@gmail.com>
2026-01-04 02:09:54 +0530

    Added rank to global leaderboard and align Redis–MongoDB data flow

--- crackcode/server/src/models/user.js
+++ crackcode/server/src/models/user.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    xp: { type: Number, default: 0 },
-    tokens: { type: Number, default: 100 },
-    rank: { type: String, default: "Rookie" },
+    totalXP: { type: Number, default: 0 },
+    level: { type: Number, default: 0 },
+    batch: { type: String, default: "Bronze" },
     lastActive: { type: Date, default: Date.now }
 });
 
