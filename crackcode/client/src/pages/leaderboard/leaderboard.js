c71d740341d1cd4b808870954ffa89e47211bfbe
Sasni Lasadi <sasnilasadi410@gmail.com>
2026-01-06 21:06:40 +0530

    Visualized global leaderboard with styling

--- crackcode/client/src/pages/leaderboard/leaderboard.js
+++
@@ -1,9 +0,0 @@
-export const fetchGlobalLeaderboard = async () => {
-  const response = await fetch("http://localhost:5050/api/leaderboard/global");
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch leaderboard");
-  }
-
-  return response.json();
-};
