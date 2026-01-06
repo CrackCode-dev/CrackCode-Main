3e60111ba4f45bfdd99fbe569947571e25f33610
Sasni Lasadi <sasnilasadi410@gmail.com>
2026-01-06 20:58:57 +0530

    Added global leaderboard API client

---
+++ crackcode/client/src/pages/leaderboard/leaderboard.js
@@ -0,0 +1,9 @@
+export const fetchGlobalLeaderboard = async () => {
+  const response = await fetch("http://localhost:5050/api/leaderboard/global");
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch leaderboard");
+  }
+
+  return response.json();
+};
