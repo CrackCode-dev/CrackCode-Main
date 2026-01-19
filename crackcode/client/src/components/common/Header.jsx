37dde8d46346ac96c0b9f4538e0a213a2b9d297d
Nadeesha Hasaranga <hasaranganadeesha2004@gmail.com>
2026-01-19 16:03:11 +0530

    Refactor routes and component imports; update Header and GameProfile

--- crackcode/client/src/components/common/Header.jsx
+++ crackcode/client/src/components/common/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import logo from '../../assets/logo/crackcode_logo.png'
 import Navbar from './Navbar'
 import { Bell , Search, UserCircle} from 'lucide-react';
@@ -13,9 +13,15 @@ const Header = ({ variant = "default" }) => {
     };
 
     const navigate = useNavigate();
+    const location = useLocation();
+
+    const hideHeaderActions = ['/', '/login','/email-verify', '/reset-password', '/gamer-profile'];
+    const showHeaderActions = !hideHeaderActions.includes(location.pathname);
+
+    const isLanding = location.pathname === '/';
 
     const handleLogoClick = () => {
-        if (variant === "landing") {
+        if (isLanding) {
             // Scroll to top of landing page
             window.scrollTo({ top: 0, behavior: 'smooth' });
         } else {
@@ -23,6 +29,10 @@ const Header = ({ variant = "default" }) => {
             navigate('/home');
         }
     };
+
+    const handleNotificationsClick = () => {
+        navigate('/gamer-profile');
+    }
 
     const userAvatar = null; // Replace with actual user avatar URL when available
   return (
@@ -39,21 +49,22 @@ const Header = ({ variant = "default" }) => {
             </div>
 
             {/* Navbar */}
-            {variant !== "landing" && (
+            {showHeaderActions && (
                 <div className='absolute left-1/2 transform -translate-x-1/2'>
                     <Navbar />
                 </div>
             )}
 
             {/* Empty div for balance (optional - for future right-side content like profile icon) */}
-            {variant !== "landing" && (
+            {showHeaderActions && (
                 <div className='flex items-center'>
                     {/* Search Bar */}
                     <input type="text" placeholder='Search Cases...' className='w-30 bg-orange-950 text-white text-sm rounded-md px-2 py-1 
                     focus:outline-none focus:ring-1 focus:ring-orange-600 focus:w-40 transition-all duration-300 ease-in-out'/>
 
                     {/* Notifications */}
-                    <Bell className='ml-6 w-6 h-6 text-white hover:text-orange-500 transition-colors duration-300' />
+                    <Bell className='ml-6 w-6 h-6 text-white hover:text-orange-500 transition-colors duration-300' 
+                    onClick={handleNotificationsClick}/>
 
                     {/* Gamer Profile Avatar*/}
                     <div className='ml-6' >
