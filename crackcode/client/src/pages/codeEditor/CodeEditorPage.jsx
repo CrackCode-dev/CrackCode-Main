59127c8c197f45d2bc91f5e37fc5de30df57ec10
Vidun-Shanuka-Upek <vidunshanusocial@gmail.com>
2026-01-25 23:14:28 +0530

    Save current work on learn module and editor

--- crackcode/client/src/pages/codeEditor/CodeEditorPage.jsx
+++ crackcode/client/src/pages/codeEditor/CodeEditorPage.jsx
@@ -31,3 +31,7 @@ export default function CodeEditorPage() {
     </EditorProvider>
   );
 }
+
+//infocrackcode_db_user
+//Up3qn0HqPFN1xQ73
+//mongodb+srv://infocrackcode_db_user:Up3qn0HqPFN1xQ73@crackcodecluster.f8j0sea.mongodb.net/?appName=crackcodeCluster
