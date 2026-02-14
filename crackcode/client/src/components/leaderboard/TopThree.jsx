bb2573a83eb72101e81934d4668e0d21d4544a65
Shenori Ruwesha <shenoriperera87@gmail.com>
2026-02-14 14:20:21 +0530

    add App.jsx file and css file to leaderboard

---
+++ crackcode/client/src/components/leaderboard/TopThree.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import LeaderboardCard from "./LeaderboardCard";
+import leaderboardData from "../../data/leaderboardData";
+
+const TopThree = () => {
+  const topThree = leaderboardData.slice(0, 3);
+
+  return (
+    <div className="top-three">
+      <LeaderboardCard user={topThree[1]} type="silver" />
+      <LeaderboardCard user={topThree[0]} type="gold" />
+      <LeaderboardCard user={topThree[2]} type="bronze" />
+    </div>
+  );
+};
+
+export default TopThree;
