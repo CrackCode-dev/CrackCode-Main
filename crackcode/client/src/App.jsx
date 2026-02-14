0f2271116602f0d52d89e6452b773e575d694f6c
Shenori Ruwesha <shenoriperera87@gmail.com>
2026-02-14 14:20:21 +0530

    add App.jsx file and css file to leaderboard

--- crackcode/client/src/App.jsx
+++ crackcode/client/src/App.jsx
@@ -1,93 +1,79 @@
-// import { Routes, Route } from 'react-router-dom'
-// import './App.css'
-// import Home from './pages/home/Home'
-// import Landing from './pages/landing/Landing'
-// import EmailVerify from './pages/userauth/EmailVerify'
-// import Login from './pages/userauth/Login'
-// import ResetPassword from './pages/userauth/ResetPassword'
-// import GameProfile from './pages/gameprofile/gameprofile'
-// import UserProfile from './pages/userprofile/userprofile'
-// import { ToastContainer } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-
-// function App() {
-//   return (
-//     <div>
-//       <ToastContainer />
-//       <Routes>
-//         <Route path='/' element={<Landing />} />
-//         <Route path='/home' element={<Home />} />
-//         <Route path='/login' element={<Login />} />
-//         <Route path='/email-verify' element={<EmailVerify />} />
-//         <Route path='/reset-password' element={<ResetPassword />} />
-//         <Route path='/game-profile' element={<GameProfile />} />
-//         <Route path='/user-profile' element={<UserProfile />} />
-//       </Routes>
-//     </div>
-//   )
-// }
-
-// export default App
-
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
+
 import Home from './pages/home/Home'
 import Landing from './pages/landing/Landing'
 import EmailVerify from './pages/userauth/EmailVerify'
 import Login from './pages/userauth/Login'
 import ResetPassword from './pages/userauth/ResetPassword'
-import CareermapMain from './pages/careermap/CareermapMain' 
-import GameProfile from './pages/gameprofile/gameprofile'
-import UserProfile from './pages/userprofile/userprofile'
+import CareermapMain from './pages/careermap/CareermapMain'
+import GameProfile from './pages/gameprofile/GameProfile'
+import UserProfile from './pages/userprofile/UserProfile'
 import CodeEditorPage from './pages/codeEditor/CodeEditorPage'
 import ProtectedRoute from './components/common/ProtectedRoute'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import CaseLogMainPage from "./pages/caselog/CaseLogMainPage";
-import CaseLogPage from "./pages/caselog/CaseLogPage";
+import CaseLogMainPage from './pages/caselog/CaseLogMainPage'
+import CaseLogPage from './pages/caselog/CaseLogPage'
+import Leaderboard from './pages/leaderboard/leaderboardPage'
 
-
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
     <div>
       <ToastContainer />
       <Routes>
+
         {/* Public routes */}
         <Route path='/' element={<Landing />} />
         <Route path='/login' element={<Login />} />
         <Route path='/verify-account' element={<EmailVerify />} />
         <Route path='/email-verify' element={<EmailVerify />} />
         <Route path='/reset-password' element={<ResetPassword />} />
-<<<<<<< HEAD
-        <Route path="/caselog" element={<CaseLogMainPage />} />
-        <Route path="/caselog" element={<CaseLogPage />} />
-        
-=======
-        <Route path='/careermaps-Main'element={<CareermapMain/>}/>
->>>>>>> 41aaf1e38a1b4fb5fdbdd5dddf2d73391a423d6e
-        
-        {/* Protected routes - require login + verified email */}
-        <Route path='/home' element={
-          // <ProtectedRoute>   //temp unwrapped to bypass auth when navigating to home(dev stage)
-            <Home />
-          // </ProtectedRoute>
-        } />
-        <Route path='/gamer-profile' element={
-          <ProtectedRoute>
-            <GameProfile />
-          </ProtectedRoute>
-        } />
-        <Route path='/user-profile' element={
-          // <ProtectedRoute>
-            <UserProfile />
-          // </ProtectedRoute>
-        } />
-        <Route path='/solve/:problemId' element={
-          <ProtectedRoute>
-            <CodeEditorPage />
-          </ProtectedRoute>
-        } />
+
+        <Route path='/caselog' element={<CaseLogMainPage />} />
+        <Route path='/caselog/details' element={<CaseLogPage />} />
+
+        <Route path='/careermaps-Main' element={<CareermapMain />} />
+        <Route path='/leaderboard' element={<Leaderboard />} />
+
+        {/* Protected routes */}
+        <Route
+          path='/home'
+          element={
+            // <ProtectedRoute>   // temporarily bypassed for dev
+              <Home />
+            // </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path='/gamer-profile'
+          element={
+            <ProtectedRoute>
+              <GameProfile />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path='/user-profile'
+          element={
+            // <ProtectedRoute>
+              <UserProfile />
+            // </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path='/solve/:problemId'
+          element={
+            <ProtectedRoute>
+              <CodeEditorPage />
+            </ProtectedRoute>
+          }
+        />
+
       </Routes>
     </div>
   )
