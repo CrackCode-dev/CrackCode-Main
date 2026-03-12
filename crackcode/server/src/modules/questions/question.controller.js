97e625d9493bdb96d3bd03702c0034df38dc646f
Shenori Ruwesha <shenoriperera87@gmail.com>
2026-03-12 13:41:58 +0530

    Fixed Error with mongodb connect shcema

--- crackcode/server/src/modules/questions/question.controller.js
+++ crackcode/server/src/modules/questions/question.controller.js
@@ -23,3 +23,29 @@ export const getQuestions = async (req, res) => {
   }
 
 };
+
+export const submitAnswer = async (req, res) => {
+
+  try {
+
+    const { questionId, answer } = req.body;
+
+    const question = await getQuestionById(questionId);
+
+    if (!question) {
+      return res.status(404).json({ message: "Question not found" });
+    }
+
+    const correct = checkAnswer(answer, question.correctAnswer);
+
+    res.json({
+      correct: correct
+    });
+
+  } catch (error) {
+
+    res.status(500).json({ message: error.message });
+
+  }
+
+};
