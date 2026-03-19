18ebe48fef89f2d8b9f407177638122f3228bc81
Nadeesha Hasaranga <hasaranganadeesha2004@gmail.com>
2026-03-19 14:15:44 +0530

    Update axios base URL and clean up whitespace in LetterGlitch component

--- crackcode/client/src/api/axios.js
+++ crackcode/client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api", // change port if needed
+  baseURL: "http://localhost:5051/api", // change port if needed
   withCredentials: true,
 });
 
