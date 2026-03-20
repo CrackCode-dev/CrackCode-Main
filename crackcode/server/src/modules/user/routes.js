c5849735f4ab721dec427d51afbf74dc676f5909
Vidun-Shanuka-Upek <vidunshanusocial@gmail.com>
2026-03-20 22:21:23 +0530

    feat(profile/auth): implement profile endpoints; add codeEditor submit service, utils

--- crackcode/server/src/modules/user/routes.js
+++ crackcode/server/src/modules/user/routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import userAuth from "../auth/middleware.js";
-import { getUserData, getProgressSummary, getUserActivity } from "./controller.js";
+import { getUserData, getProgressSummary, getUserActivity, getUserProgressRaw } from "./controller.js";
 
 const router = express.Router();
 
 router.get("/data", userAuth, getUserData);
 router.get("/progress-summary", userAuth, getProgressSummary);
 router.get("/activity", userAuth, getUserActivity);
+router.get("/progress-raw", userAuth, getUserProgressRaw);
 
 export default router;
